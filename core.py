e9bc6d2997a1f741dcb0fcbcc221dde7031ee0c2
Amir Ayub <dev.amirayub@gmail.com>
2019-07-25 21:54:50 +0300

    bug-fixes

--- core.py
+++ core.py
@@ -3,16 +3,16 @@ import json
 import urllib.parse as urlparse
 from urllib.parse import urlencode
 
+params = {'rounds': '9', 'text': 'amir'}
 url = "http://hash-service.eastus2.azurecontainer.io/hash"
-params = {'rounds': '4', 'text': 'amir'}
 url_parts = list(urlparse.urlparse(url))
 query = dict(urlparse.parse_qsl(url_parts[4]))
 query.update(params)
 url_parts[4] = urlencode(query)
 url = urlparse.urlunparse(url_parts)
 
-for i in range(25):
+for i in range(10):
     response = requests.get(url)
     js = json.loads(response.text)
-    print(js["hash"])
+    print(js["host_name"])
 
